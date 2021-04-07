b567bd3e0a35d2933854e8b0a10dd6d0d571754a
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-04-07 14:16:28 +0800

    adding Express and NPM and gitignore

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 const htmlRouter = require('./routes/html-routes');
 const apiRouter = require('./routes/api-routes');
+
+app.use(express.static(__dirname+'/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+htmlRouter(app);
+apiRouter(app);
+
+app.listen(PORT, function () {
+    console.log('Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
+  });
